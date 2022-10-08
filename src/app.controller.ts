7e9d93aa660cb76e316d622dde076f90ab1ccef9
VladKost999 <russia7780030513@gmail.com>
2022-10-08 19:44:37 +0300

    Добавил ВСЁ

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { CreateCatDto } from './cats.dto';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  GetHello(): string {
+    return "WELCOME TO THE CLUB , BUDDY"
+  }
+  
+  @Get(":cats")
+  GetCats(): string {
+    return "Ты не кот!"
+  }
+
+  @Get(":id")
+  GetCatsId(@Param() params): string {
+    let id = params.id
+    return `This action returns a ${id} cat`;
+  }
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+}
