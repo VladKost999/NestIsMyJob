edd6d4bdba0d298c3b40b0f22359871d39d84574
VladKost999 <russia7780030513@gmail.com>
2022-10-09 17:19:15 +0300

    Фигню сделал

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,29 +1,14 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import { CreateCatDto } from './cats.dto';
+import { CatsService } from './cats/cats.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private appService: AppService, private catService: CatsService) {}
 
-  @Get()
-  GetHello(): string {
-    return "WELCOME TO THE CLUB , BUDDY!!!"
-  }
-  
-  @Get(":cats")
-  GetCats(): string {
-    return "Ты не кот!"
-  }
+    @Get(":cats")
+    getCats() {
+      return this.catService.getCats()
+    }
 
-  @Get(":id")
-  GetCatsId(@Param() params): string {
-    let id = params.id
-    return `This action returns a ${id} cat`;
-  }
-
-  @Post()
-  async create(@Body() createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
-  }
 }
